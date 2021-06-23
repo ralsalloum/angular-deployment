f0b3646aa57c7e18ca0214e7fdea1638c6dbc9b0
Rami AlSalloum <ramisalloum12@hotmail.com>
2021-06-23 18:24:53 +0300

    swagger-ui-dist was being used

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SwaggerUiComponent } from './swagger-ui/swagger-ui.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SwaggerUiComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/swagger-ui/swagger-ui.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const SwaggerUIBundle: any;
+
+@Component({
+  selector: 'app-swagger-ui',
+  templateUrl: './swagger-ui.component.html',
+  styleUrls: ['./swagger-ui.component.css']
+})
+export class SwaggerUiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const ui = SwaggerUIBundle({
+      dom_id: '#swagger-ui',
+      layout: 'BaseLayout',
+      presets: [
+        SwaggerUIBundle.presets.apis,
+        SwaggerUIBundle.SwaggerUIStandalonePreset
+      ],
+      url: 'http://161.35.29.131/doc.json',
+      docExpansion: 'none',
+      operationsSorter: 'alpha'
+    });
+  }
+
+}
