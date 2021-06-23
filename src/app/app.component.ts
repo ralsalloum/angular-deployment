6ee6cdc8197021979c3a51d7c932afa7aab98d62
Rami AlSalloum <ramisalloum12@hotmail.com>
2021-06-23 19:56:51 +0300

    swagger-ui component removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare const SwaggerUIBundle: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'swagger-ui-dist-angular';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const ui = SwaggerUIBundle({
+      dom_id: '#swagger-ui',
+      layout: 'BaseLayout',
+      presets: [
+        SwaggerUIBundle.presets.apis,
+        SwaggerUIBundle.SwaggerUIStandalonePreset
+      ],
+      url: 'http://161.35.29.131/doc.json',
+      docExpansion: 'none',
+      operationsSorter: 'alpha'
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SwaggerUiComponent } from './swagger-ui/swagger-ui.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SwaggerUiComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
